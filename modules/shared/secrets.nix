7fb4298fb3f6396eb0df05b41e3cf674b51c9db0
Kevin Neely <kevin.r.neely@outlook.com>
2024-07-03 01:00:15 -0400

    t

---
+++ modules/shared/secrets.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, agenix, secrets, ... }:
+
+let user = "kevin"; in
+{
+  age.identityPaths = [
+    "/home/${user}/.ssh/id_ed25519"
+  ];
+
+  # Your secrets go here
+  #
+  # Note: the installWithSecrets command you ran to boostrap the machine actually copies over
+  #       a Github key pair. However, if you want to store the keypair in your nix-secrets repo
+  #       instead, you can reference the age files and specify the symlink path here. Then add your
+  #       public key in shared/files.nix.
+  #
+  #       If you change the key name, you'll need to update the SSH extraConfig in shared/home-manager.nix
+  #       so Github reads it correctly.
+
+  #"tailscale-auth-client-id.age".publicKeys = [kevin];
+  # "tailscale-auth-key.age".publicKeys = [kevin];
+
+  age.secrets."tailscale-auth-key" = {
+    symlink = false;
+    path = "/home/${user}/.secrets/tailscale-auth-key";
+    file =  "${secrets}/github-ssh-key.age";
+    mode = "600";
+    owner = "${user}";
+    group = "wheel";
+  };
+
+}
