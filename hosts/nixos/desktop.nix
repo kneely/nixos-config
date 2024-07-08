15a6019f17c90ba9f25459e577f7825f8bc10b06
Kevin Neely <kevin.r.neely@outlook.com>
2024-07-07 22:59:22 -0400

    t

--- hosts/nixos/desktop.nix
+++ hosts/nixos/desktop.nix
@@ -297,14 +297,15 @@ in
   virtualisation = {
     containers = {
       # enable = true;
-      cdi.dynamic.nvidia.enable = true;
+      # cdi.dynamic.nvidia.enable = true;
     };
     docker = {
       enable = true;
       # CDI is feature-gated and only available from Docker 25 and onwards
       package = pkgs.docker_25;
       daemon.settings.features.cdi = true;
-      storageDriver = "zfs";
+      # storageDriver = "zfs";
+      logDriver = "json-file";
     };
     oci-containers.backend = "docker";
   };
