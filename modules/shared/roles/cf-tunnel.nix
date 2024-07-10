ddbbb6564c8d8a0474d486c4bce94fa01c1ccdb4
Kevin Neely <kevin.r.neely@outlook.com>
2024-07-10 10:00:44 -0400

    t

---
+++ modules/shared/roles/cf-tunnel.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, lib, ... }:
+with lib;
+let cfg = config.roles.cf-tunnel;
+in
+{
+  options.roles.cf-tunnel = with types; {
+    enable = mkEnableOption "Enable Cloudflare Tunnel";
+  };
+
+  config = mkIf cfg.enable {
+    age.secrets.cf-tunnel.file = "${secrets}/cloudflare-tunnel.age";
+    users.groups.cloudflared = { };
+
+    users.users.cloudflared = {
+      group = "cloudflared";
+      isSystemUser = true;
+    };
+
+    services.cloudflared = {
+      enable = true;
+      group = "cloudflare";
+      user = "cloudflared";
+      tunnels = {
+        "00000000-0000-0000-0000-000000000000" = {
+          credentialsFile = config.age.secrets.cf-tunnel.path;
+          default = "http_status:404";
+          ingress = {
+            "chat.neelyinno.com" = {
+              service = "http://localhost:3000";
+            };
+          };
+        };
+      };
+    };
+  };
+}
