59f3db7e690af93fbae9155ab52e9561d9968d63
Kevin Neely <kevin.r.neely@outlook.com>
2024-07-10 10:04:47 -0400

    t

--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -10,7 +10,7 @@
   # };
 
   # Initializes Emacs with org-mode so we can tangle the main config
-  ".emacs.d/init.el" = {
-    text = builtins.readFile ../shared/config/emacs/init.el;
-  };
+  # ".emacs.d/init.el" = {
+  #   text = builtins.readFile ../shared/config/emacs/init.el;
+  # };
 }
