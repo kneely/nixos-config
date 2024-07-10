b8adc5336aef1f1d78125f720d95d0ed5539478f
Kevin Neely <kevin.r.neely@outlook.com>
2024-07-09 23:56:50 -0400

    t

--- modules/shared/roles/ai.nix
+++ modules/shared/roles/ai.nix
@@ -29,19 +29,19 @@ in
       };
 
       # docker run -d -p 3000:8080 --gpus all --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:cuda
-      # open-webui = {
-      #   image = "ghcr.io/open-webui/open-webui:cuda";
-      #   ports = [ "3000:8080" ];
-      #   volumes = [ "${dockerDataDir}/open-webui:/app/backend/data" ];
-      #   extraOptions = [ "--gpus=all"  ];
-      #   environment = {
-      #     OLLAMA_BASE_URL = "http://ollama:11434";
-      #     USE_CUDA_DOCKER = "true";
-      #     NVIDIA_VISIBLE_DEVICES = "all";
-      #     NVIDIA_DRIVER_CAPABILITIES = "all";
+      open-webui = {
+        image = "ghcr.io/open-webui/open-webui:cuda";
+        ports = [ "3000:8080" ];
+        volumes = [ "${dockerDataDir}/open-webui:/app/backend/data" ];
+        extraOptions = [ "--gpus=all"  ];
+        environment = {
+          OLLAMA_BASE_URL = "http://ollama:11434";
+          USE_CUDA_DOCKER = "true";
+          NVIDIA_VISIBLE_DEVICES = "all";
+          NVIDIA_DRIVER_CAPABILITIES = "all";
 
-      #   };
-      # };
+        };
+      };
     };
   };
 }
