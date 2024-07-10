d5a3a3b710c63de322a70c3755874aa80c47d7fd
Kevin Neely <kevin.r.neely@outlook.com>
2024-07-09 23:48:00 -0400

    t

--- modules/shared/roles/ai.nix
+++ modules/shared/roles/ai.nix
@@ -30,7 +30,7 @@ in
 
       # docker run -d -p 3000:8080 --gpus all --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:cuda
       open-webui = {
-        image = "ghcr.io/open-webui/open-webui:cuda";
+        image = "ghcr.io/open-webui/open-webui:main";
         ports = [ "3000:8080" ];
         volumes = [ "${dockerDataDir}/open-webui:/app/backend/data" ];
         environment = {
