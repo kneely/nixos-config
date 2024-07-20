7dc44d486f9edaa8792c4d77004d4f8a73b44bfd
Kevin Neely <kevin.r.neely@outlook.com>
2024-07-20 17:13:33 -0400

    t

--- modules/shared/roles/tailscale-submodule.nix
+++ modules/shared/roles/tailscale-submodule.nix
@@ -111,7 +111,8 @@ let
           "TS_HOSTNAME" = cfg.TShostname;
           "TS_STATE_DIR" = "/var/lib/tailscale";
           "TS_EXTRA_ARGS" = "--advertise-tags=" + formatTags + " " + cfg.TSargs;
-          "TS_AUTHKEY" = config.age.secrets.tailscale.path;
+          # "TS_AUTHKEY" = config.age.secrets.tailscale.path;
+          "TS_AUTHKEY" = "file:${config.age.secrets.tailscale.path}";
       }
       (lib.mkIf (cfg.TSserve != {}) {
           "TS_SERVE_CONFIG" = "config/tailscaleCfg.json";
