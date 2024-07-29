44618d77751389c8e1c688733cd6102c2489182e
Kevin Neely <kevin.r.neely@outlook.com>
2024-07-29 10:42:25 -0400

    t

--- modules/shared/roles/ai.nix
+++ modules/shared/roles/ai.nix
@@ -50,6 +50,9 @@ in
       virtualHosts."nixos.tail103fe.ts.net".extraConfig = ''
         reverse_proxy 127.0.0.1:3000 
       '';
+      virtualHosts."nixos-open-webui.tail103fe.ts.net".extraConfig = ''
+        reverse_proxy 127.0.0.1:3000 
+      '';
     };
 
     roles.tsfunnel.tailscaled = {
