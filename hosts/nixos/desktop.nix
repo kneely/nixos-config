cba55af6facc5dc4112e593022026870daa745fa
Kevin Neely <kevin.r.neely@outlook.com>
2024-07-31 16:00:24 -0400

    t

--- hosts/nixos/desktop.nix
+++ hosts/nixos/desktop.nix
@@ -21,9 +21,9 @@ in
   # services.mediaserver.enable = true;
   roles.pirate.enable = true;
   # roles.authelia.enable = true;
-  roles.tailscale.enable = true;
+  # roles.tailscale.enable = true;
   roles.ai.enable = true;
-  roles.cf-tunnel.enable = true;
+  # roles.cf-tunnel.enable = true;
 
   nixpkgs.config.cudaSupport = true;
   systemd.enableUnifiedCgroupHierarchy = false;
