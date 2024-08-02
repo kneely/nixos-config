b85469c1ff7b23181c857ded857696e54ebfb98c
Kevin Neely <kevin.r.neely@outlook.com>
2024-08-02 17:49:37 -0400

    t

--- modules/shared/roles/tailscale-service.nix
+++ modules/shared/roles/tailscale-service.nix
@@ -8,25 +8,51 @@ in
   };
 
   config = mkIf cfg.enable {
-    # Enable tailscale daemon.
-    services.tailscale = {
-      enable = true;
-      interfaceName = "tailscale0";
+    environment.systemPackages = [ pkgs.tailscale ];
 
-      # permitCertUid = "caddy";
+    services.tailscale.enable = true;
 
-      authKeyFile = config.age.secrets.tailscale.path;
-      extraUpFlags = [ "--ssh" "--advertise-tags" ];
-    };
 
     age.secrets.tailscale.file = "${secrets}/tailscale-auth-key.age";
 
+systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up --authkey "${secrets}/tailscale-auth-key.age" --ssh --advertise-tags="${config.services.tailscale.tags}"
+      '';
+    };
     networking.firewall = {
-      # Trust inbound tailnet traffic.
+      # enable the firewall
+      enable = true;
+
+      # always allow traffic from your Tailscale network
       trustedInterfaces = [ "tailscale0" ];
 
-      # Allow tailscale through firewall.
+      # allow the Tailscale UDP port through the firewall
       allowedUDPPorts = [ config.services.tailscale.port ];
+
+      # allow you to SSH in over the public internet
+      allowedTCPPorts = [ 22 ];
     };
   };
 }
