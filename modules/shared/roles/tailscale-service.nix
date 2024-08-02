7f56f5618755a504daac26ad68e360e98f36d957
Kevin Neely <kevin.r.neely@outlook.com>
2024-08-02 17:34:03 -0400

    t

--- modules/shared/roles/tailscale-service.nix
+++ modules/shared/roles/tailscale-service.nix
@@ -16,7 +16,7 @@ in
       # permitCertUid = "caddy";
 
       authKeyFile = config.age.secrets.tailscale.path;
-      extraUpFlags = [ "--ssh " ];
+      extraUpFlags = [ "--ssh" ];
     };
 
     age.secrets.tailscale.file = "${secrets}/tailscale-auth-key.age";
