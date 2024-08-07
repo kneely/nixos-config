c761012f852976be735cf34e0e6021627a458ced
Kevin Neely <kevin.r.neely@outlook.com>
2024-08-07 18:05:40 -0400

    t

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Starter Configuration with secrets for MacOS and NixOS";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     agenix.url = "github:ryantm/agenix";
     # home-manager.url = "github:nix-community/home-manager";
     home-manager = {
