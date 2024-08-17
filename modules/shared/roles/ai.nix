bb2c2197e04294a81a8da90fccc09e35e16d53a2
Kevin Neely <kevin.r.neely@outlook.com>
2024-08-16 23:36:33 -0400

    t

--- modules/shared/roles/ai.nix
+++ modules/shared/roles/ai.nix
@@ -19,35 +19,47 @@ in
       "d ${dockerDataDir}/open-webui 0770 - - - -"
     ];
 
+    services.ollama = {
+      #package = pkgs.unstable.ollama; # Uncomment if you want to use the unstable channel, see https://fictionbecomesfact.com/nixos-unstable-channel
+      enable = true;
+      acceleration = "cuda"; # Or "rocm"
+      #environmentVariables = { # I haven't been able to get this to work myself yet, but I'm sharing it for the sake of completeness
+        # HOME = "/home/ollama";
+        # OLLAMA_MODELS = "/home/ollama/models";
+        # OLLAMA_HOST = "0.0.0.0:11434"; # Make Ollama accesible outside of localhost
+        # OLLAMA_ORIGINS = "http://localhost:8080,http://192.168.0.10:*"; # Allow access, otherwise Ollama returns 403 forbidden due to CORS
+      #};
+    };
+
     virtualisation.oci-containers.containers = {
       # docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
-      ollama = {
-        image = "ollama/ollama:latest";
-        ports = [ "11434:11434" ];
-        volumes = [ "${dockerDataDir}/ollama:/root/.ollama" ];
-        extraOptions = [ "--gpus=all" "--pull=newer"  ];
-        environment = {
-          NVIDIA_VISIBLE_DEVICES = "all";
-          NVIDIA_DRIVER_CAPABILITIES = "all";
-          OLLAMA_ORIGINS = "*";
-          # PUID = "600";
-          # PGID = "600";
-        };
-      };
+      # ollama = {
+      #   image = "ollama/ollama:latest";
+      #   ports = [ "11434:11434" ];
+      #   volumes = [ "${dockerDataDir}/ollama:/root/.ollama" ];
+      #   extraOptions = [ "--gpus=all" "--pull=newer"  ];
+      #   environment = {
+      #     NVIDIA_VISIBLE_DEVICES = "all";
+      #     NVIDIA_DRIVER_CAPABILITIES = "all";
+      #     OLLAMA_ORIGINS = "*";
+      #     # PUID = "600";
+      #     # PGID = "600";
+      #   };
+      # };
 
       # loadModels = ["llama3" "gemma2" "codegemma" "llama2-uncensored" "phi3" "deepseek-coder-v2" "qwen2"];
 
       # docker run -d -p 3000:8080 --gpus all --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:cuda
       open-webui = {
-        image = "ghcr.io/open-webui/open-webui:cuda";
+        image = "ghcr.io/open-webui/open-webui:main";
         ports = [ "3000:8080" ];
         volumes = [ "${dockerDataDir}/open-webui:/app/backend/data" ];
         extraOptions = [ "--gpus=all" "--pull=newer"  ];
         environment = {
-          OLLAMA_BASE_URL = "http://ollama:11434";
-          USE_CUDA_DOCKER = "true";
-          NVIDIA_VISIBLE_DEVICES = "all";
-          NVIDIA_DRIVER_CAPABILITIES = "all";
+          OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+          # USE_CUDA_DOCKER = "true";
+          # NVIDIA_VISIBLE_DEVICES = "all";
+          # NVIDIA_DRIVER_CAPABILITIES = "all";
         };
       };
     };
