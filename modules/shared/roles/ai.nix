c548f5bf886d013a9249952c0004abec078f09ac
Kevin Neely <kevin.r.neely@outlook.com>
2024-08-17 01:00:33 -0400

    t

--- modules/shared/roles/ai.nix
+++ modules/shared/roles/ai.nix
@@ -31,60 +31,15 @@ in
       #};
     };
 
-    virtualisation.oci-containers.containers = {
-      # docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
-      # ollama = {
-      #   image = "ollama/ollama:latest";
-      #   ports = [ "11434:11434" ];
-      #   volumes = [ "${dockerDataDir}/ollama:/root/.ollama" ];
-      #   extraOptions = [ "--gpus=all" "--pull=newer"  ];
-      #   environment = {
-      #     NVIDIA_VISIBLE_DEVICES = "all";
-      #     NVIDIA_DRIVER_CAPABILITIES = "all";
-      #     OLLAMA_ORIGINS = "*";
-      #     # PUID = "600";
-      #     # PGID = "600";
-      #   };
-      # };
-
-      # loadModels = ["llama3" "gemma2" "codegemma" "llama2-uncensored" "phi3" "deepseek-coder-v2" "qwen2"];
-
-      # docker run -d -p 3000:8080 --gpus all --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:cuda
-      open-webui = {
-        image = "ghcr.io/open-webui/open-webui:main";
-        # ports = [ "3000:8080" ];
-        volumes = [ "${dockerDataDir}/open-webui:/app/backend/data" ];
-        extraOptions = [ "--pull=newer" "--network=host" ];
-        environment = {
-          PORT = "9090";
-          OLLAMA_BASE_URL = "http://localhost:11434";
-          # USE_CUDA_DOCKER = "true";
-          # NVIDIA_VISIBLE_DEVICES = "all";
-          # NVIDIA_DRIVER_CAPABILITIES = "all";
-        };
+    services.open-webui = {
+      enable = true;
+      port = 3000;
+      openFirewall = true;
+      environmentVariables = {
+        OLLAMA_BASE_URL = "http://localhost:11434";
       };
     };
 
-    # services.caddy = {
-    #   enable = true;
-    #   virtualHosts."nixos.tail103fe.ts.net".extraConfig = ''
-    #     reverse_proxy 127.0.0.1:3000 
-    #   '';
-    #   virtualHosts."nixos-open-webui.tail103fe.ts.net".extraConfig = ''
-    #     reverse_proxy 127.0.0.1:3000 
-    #   '';
-    # };
 
-    # roles.tsfunnel.tailscaled = {
-    #   TSopen-webui = {
-    #     enable = true;
-    #     imageVersion = "latest";
-    #     TSserve = {
-    #       "/" = "http://open-webui:8080";
-    #     };
-    #     enableFunnel = true;
-    #     tags = ["tag:services"];
-    #   };
-    # };
   };
 }
